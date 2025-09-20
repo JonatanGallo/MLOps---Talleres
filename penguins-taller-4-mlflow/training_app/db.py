9b0089b5072b27ed7132eb33c4811e5a7c1efdd7
jdromero9402 <jdromero9402@gmail.com>
2025-09-19 23:48:45 -0500

    add training app

--- penguins-taller-4-mlflow/training_app/db.py
+++ penguins-taller-4-mlflow/training_app/db.py
@@ -5,11 +5,11 @@ import os
 import pandas as pd
 
 def get_db_connection():
-    host = os.getenv('DB_HOST', 'mysql_db')  # service name in docker-compose
-    port = int(os.getenv('DB_PORT', '3306'))
+    host = os.getenv('DB_HOST', '10.43.100.99')  # service name in docker-compose
+    port = int(os.getenv('DB_PORT', '8005'))
     user = os.getenv('DB_USER', 'user')
     password = os.getenv('DB_PASSWORD', 'password')
-    database = os.getenv('DB_NAME', 'training')
+    database = os.getenv('DB_NAME', 'training') 
 
     return mysql.connector.connect(
         host=host,

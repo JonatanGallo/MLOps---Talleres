95d173659690d6c04032d3deba8d7a134445625d
Juan Camilo Torres Peña <skamilo9406@gmail.com>
2025-09-30 21:27:42 -0500

    actualiza etl para el dataset Cover Type

--- proyecto_2/training_app/etl.py
+++ proyecto_2/training_app/etl.py
@@ -10,34 +10,13 @@ from .db import create_table, insert_data, get_rows, clear_table, get_rows_with_
 endl = "#" * 100
 
 
-def feature_engineering(penguins):
-  """Applies feature scaling and other transformations to numerical features."""
-  numerical_features = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']
-  scaler = StandardScaler()
-  penguins[numerical_features] = scaler.fit_transform(penguins[numerical_features])
-  joblib.dump(scaler, "scaler.pkl")
-  return penguins
-
 # Cleans, transforms, encodes, and scales the penguins dataset
-def clean_data(penguins):
-  penguins['bill_length_mm'] = pd.to_numeric(penguins['bill_length_mm'], errors='coerce')
-  penguins['bill_length_mm'] = penguins['bill_length_mm'].fillna(penguins['bill_length_mm'].mean())
-  penguins['sex'] = penguins['sex'].fillna('Unknown')
-  penguins = pd.get_dummies(penguins, columns=['island', 'sex'], drop_first=True)
-  # Convert boolean columns to 0/1 integers
-  bool_cols = penguins.select_dtypes(include=['bool']).columns
-  penguins[bool_cols] = penguins[bool_cols].astype(int)
-  penguins = feature_engineering(penguins)
+def clean_data(covertype_data):
+  covertype_data = pf.get_dummies(covertype_data, columns=['Wilderness_Area','Soil_Type'], drop_first=True)
   # Drop any rows that still have NaNs after imputation and encoding
-  penguins.dropna(inplace=True)
-
-  print("Null values", penguins.isnull().sum())
-  print(endl)
-  print("Data types", penguins.dtypes)
-  print(endl)
-  print("Species", penguins['species'].value_counts(normalize=True))
-  print(endl)
-  return penguins
+  covertype_data.dropna(inplace=True)
+  print("data after cleaning", covertype_data.head())
+  return covertype_data
 
 def show_initial_data(penguins):
   print("Initial data")
@@ -54,19 +33,7 @@ def show_after_cleaning(X, y):
   print(y.shape)
   print(endl)
 
-def get_data():
-  penguins = load_penguins()
-  show_initial_data(penguins)
-  # clean the data with NaN values
-  penguins = clean_data(penguins)
-
-  le = LabelEncoder()
-  y = pd.Series(le.fit_transform(penguins['species']), index=penguins.index, name='species')
-  X = penguins.drop(columns=["species", "year"])
-  show_after_cleaning(X, y)
-  return X, y
-
-def store_raw_data():
+def store_raw_data(dataFrame):
   penguins = load_penguins()
   penguins.to_csv("raw_data.csv", index=False)
   create_table("raw_data", penguins)
@@ -76,9 +43,7 @@ def clear_raw_data():
   delete_table("raw_data")
 
 def get_raw_data():
-  penguins = load_penguins()
-  rows = get_rows("raw_data")
-  columns = penguins.columns.tolist()
+  rows, columns = get_rows_with_columns("raw_data")
   df = pd.DataFrame([row[1:] for row in rows], columns=columns)
   print(df.head())
   return df
@@ -95,11 +60,10 @@ def save_clean_data():
 
 def get_clean_data():
   rows, columns = get_rows_with_columns("clean_data")
-  columns = columns[1:]  # remove id column
   le = LabelEncoder()
   clean_data_df = pd.DataFrame([row[1:] for row in rows], columns=columns)
-  y = pd.Series(le.fit_transform(clean_data_df['species']), index=clean_data_df.index, name='species')
-  X = clean_data_df.drop(columns=["species", "year"])
+  y = clean_data_df['Cover_Type']
+  X = clean_data_df.drop(columns=["Cover_Type"])
 
   show_after_cleaning(X, y)
   return X, y

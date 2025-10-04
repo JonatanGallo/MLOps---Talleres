248c9d183da1b267cf90cec63fd5cb29230c94b4
Juan Camilo Torres <skamilo9406@gmail.com>
2025-10-04 00:49:28 -0500

    changes in Dag to only run 10 times

--- proyecto_2/dags/training.py
+++ proyecto_2/dags/training.py
@@ -1,11 +1,30 @@
-from airflow import DAG
+from airflow import DAG, settings
+import subprocess
 from datetime import datetime, timedelta
+from airflow.utils.state import State
 from training_app.etl import store_raw_data, clean_all_data, get_raw_data, save_clean_data, get_clean_data
 from training_app.train import trainModel
 from airflow.operators.python import PythonOperator, ShortCircuitOperator, BranchPythonOperator
-from airflow.models import Variable
+from airflow.models import Variable, DagRun
 from airflow.operators.empty import EmptyOperator
 
+
+def pause_dag_if_failed(**context):
+    dag_id = context['dag'].dag_id
+    session = settings.Session()
+    dag_runs = session.query(DagRun).filter(DagRun.dag_id == dag_id).order_by(DagRun.execution_date.desc()).all()
+    subprocess.run(["airflow", "dags", "pause", dag_id])
+
+def check_run_count(**context):
+    # keep track of how many runs have happened
+     key = "training_dag_run_count"
+     count = int(Variable.get(key, default_var="0"))
+     print(f"Current run count: {count}")
+     if count >=10:
+        raise ValueError("Reached maximum number of runs (10).")
+    # increment counter
+     Variable.set(key, str(count + 1))
+     print(f"Run {count+1}/10")
 
 def check_first_run():
     if Variable.get("dag_first_run_done", default_var="false") == "false":
@@ -42,9 +61,10 @@ def mark_first_run_done():
 with DAG (dag_id="training_dag",
         description="Entrenando modelos",
         schedule_interval=timedelta(minutes=5, seconds=20),   # every 5 minutes and 20 seconds
-        start_date=datetime(2025, 10, 2, 0, 0, 0),   # change as needed
+        start_date=datetime(2025, 10, 3, 0, 0, 0),   # change as needed
         catchup=False,
-        max_active_runs=10
+        max_active_runs=10,
+        is_paused_upon_creation=False
 ) as dag:
 
     mark_done = PythonOperator(
@@ -52,6 +72,13 @@ with DAG (dag_id="training_dag",
         python_callable=mark_first_run_done
     )
 
+    pause_dag_task = PythonOperator(
+        task_id="pause_dag_if_failed",
+        python_callable=pause_dag_if_failed,
+        provide_context=True,
+        trigger_rule="one_failed",  # Se ejecuta si alguna tarea falla
+    )
+     
     branch = BranchPythonOperator(
         task_id="branch_first_run",
         python_callable=choose_branch,
@@ -87,5 +114,6 @@ with DAG (dag_id="training_dag",
         trigger_rule="none_failed_min_one_success",
     )
 
-    check >> branch >> [clean_all_data, skip_first_time]>> join_after_branch >> mark_done >> t2 >> t3 >> t5 >> t6
+    #check >> branch >> [clean_all_data, skip_first_time]>> join_after_branch >> mark_done >> t2 >> t3 >> t5 >> t6
+    check >> branch >> [clean_all_data, skip_first_time]>> join_after_branch >> mark_done >> t2 >> t3 >> t5 >> t6 >> pause_dag_task
     

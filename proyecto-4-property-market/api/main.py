ae8c198ccd2dfcde6c9eb152b61d3cd0dd7c2e26
Jonatan Gallo <jonatan.gallo262@gmail.com>
2025-11-26 23:28:07 -0500

    change name API

--- proyecto-4-property-market/api/main.py
+++ proyecto-4-property-market/api/main.py
@@ -30,7 +30,7 @@ async def lifespan(app: FastAPI):
     yield  # <-- this yields control to the app runtime
     print("🧹 Cleaning up resources at shutdown...")
 
-app = FastAPI(title="Diabetes API", version="1.0", lifespan=lifespan)
+app = FastAPI(title="Property Market", version="1.0", lifespan=lifespan)
 
 
 @app.get("/models")
